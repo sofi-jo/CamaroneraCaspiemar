19a9c53845364467ec8494d61caf0db44bbb0366
MiguelCaraguay <miguelcaraguay.1502@gmail.com>
2021-12-26 12:42:25 -0500

    proyecto

---
+++ proyecto/dll/config.php
@@ -0,0 +1,10 @@
+<?php
+$nombreSitio="PODOCARPUS";
+$urlSitio="http://127.0.0.1/Clases/ingenieriaWeb/Prueba/";
+
+//Datos de la base de datos
+	define('DBHOST','127.0.0.1');
+	define('DBUSER','root');
+	define('DBPASS','');
+	define('DBNAME','prueba1');
+?>
