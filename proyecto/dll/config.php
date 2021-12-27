75e4f0787eaae46546c6b211c5b33cea5d142c1b
msmartinez12 <msmartinez12@utpl.edu.ec>
2021-12-27 18:33:47 -0500

    prueba cambios

--- proyecto/dll/config.php
+++ proyecto/dll/config.php
@@ -1,10 +1,8 @@
 <?php
-$nombreSitio="PODOCARPUS";
-$urlSitio="http://127.0.0.1/Clases/ingenieriaWeb/Prueba/";
 
 //Datos de la base de datos
 	define('DBHOST','127.0.0.1');
 	define('DBUSER','root');
 	define('DBPASS','');
-	define('DBNAME','prueba1');
+	define('DBNAME','mydb');
 ?>
